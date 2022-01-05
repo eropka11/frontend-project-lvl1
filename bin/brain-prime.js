40713a7b6f64aaa2b897f1a96ea77f0a438d72d9
Егор <tujh1993@mail.ru>
2022-01-05 17:14:07 +0300

    smells fix

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -2,7 +2,7 @@
 /* eslint-disable import/extensions */
 
 import { greetings } from '../src/index.js';
-import { gamePrime } from '../src/games/prime.js';
+import gamePrime from '../src/games/prime.js';
 
 const name = greetings();
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -2,7 +2,7 @@
 /* eslint-disable import/extensions */
 
 import { greetings } from '../src/index.js';
-import { gameProgression } from '../src/games/progression.js';
+import gameProgression from '../src/games/progression.js';
 
 const name = greetings();
 console.log('What number is missing in the progression?');
